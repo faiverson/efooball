85571a5211dc725f11d70883f35fe77b8cabe5c4
Fabian Torres <fa.iverson@gmail.com>
2025-12-03 19:32:07 -0300

    create a full dump from scratch

--- .fly/scripts/01-migrate.sh
+++ .fly/scripts/01-migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Running migrations..."
-php /var/www/html/artisan migrate --force
+php /var/www/html/artisan migrate:fresh --force
 echo "Running seeders..."
 php /var/www/html/artisan db:seed --force
